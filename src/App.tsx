04b6cab52f52c9030e5f478cb63a15434829144e
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-09-22 22:01:12 +0530

    icons

--- src/App.tsx
+++ src/App.tsx
@@ -162,12 +162,9 @@ export default function BeatVisualizer() {
       className="w-screen h-screen flex items-center justify-center transition-all relative"
     >
       {!file ? (
-        <div className="items-center gap-2 bg-transparent">
-          <h1 className="text-white text-4xl font-bold mb-6 text-center p-4">
-            Love music ?
-          </h1>
+        <div className="bg-transparent">
           <label className="cursor-pointer text-white text-[30px] px-6 py-3 w-full h-full flex flex-row-reverse items-center justify-center gap-2 border-2 border-gray-400 rounded-lg">
-            <div className="text-[15px]">MP3...</div>
+            <div className="text-[15px]">Upload MP3 ( to visualize )</div>
             <MdOutlineFileUpload />
             <input
               type="file"
@@ -179,7 +176,7 @@ export default function BeatVisualizer() {
                 }
               }}
             />
-            
+
           </label>
         </div>
       ) : (
