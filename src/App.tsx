61813e085bb13e73dcfba1333437ab010c7a7480
Rahul Baradol <rahul.baradol.14@gmail.com>
2026-05-09 23:50:13 +0530

    fix: disable hardware check

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { lazy, Suspense, useEffect, useRef } from 'react';
+import { lazy, Suspense } from 'react';
 import { AnimationLayout } from '@/layouts/animation-layout';
 import { StudioLayout } from './layouts/studio-layout';
 
@@ -9,52 +9,52 @@ const OpenmicStudio = lazy(() => import('./pages/studio/openmic'));
 const Metronome = lazy(() => import('./pages/studio/metronome'));
 const Looper = lazy(() => import('./pages/studio/looper'));
 
-import { useAppStore } from './store/use-app-store';
-import { StudioActivationStatus } from './store/schema';
+// import { useAppStore } from './store/use-app-store';
+// import { StudioActivationStatus } from './store/schema';
 import GradientDesign from './pages/gradient';
 
 function App() {
-    const { setActivateStudio } = useAppStore();
+    // const { setActivateStudio } = useAppStore();
 
-    const animationFrameRef = useRef<number | null>(null);
+    // const animationFrameRef = useRef<number | null>(null);
 
-    useEffect(() => {
-        let start: number | null = null;
-        let sum: number = 0;
-        let count: number = 0;
+    // useEffect(() => {
+    //     let start: number | null = null;
+    //     let sum: number = 0;
+    //     let count: number = 0;
 
-        function measureAnimationFrameIntervals() {
-            if (count == 120) {
-                const avgTime = sum / count;
-                if (avgTime <= 17) {
-                    setActivateStudio(StudioActivationStatus.ACTIVE);
-                } else {
-                    setActivateStudio(StudioActivationStatus.INACTIVE);
-                }
-                return;
-            } 
+    //     function measureAnimationFrameIntervals() {
+    //         if (count == 120) {
+    //             const avgTime = sum / count;
+    //             if (avgTime <= 17) {
+    //                 setActivateStudio(StudioActivationStatus.ACTIVE);
+    //             } else {
+    //                 setActivateStudio(StudioActivationStatus.INACTIVE);
+    //             }
+    //             return;
+    //         } 
 
-            if (start == null) {
-                start = performance.now();
-                requestAnimationFrame(measureAnimationFrameIntervals);
-            } else {
-                const now = performance.now();
-                const diff = now - start!;
-                sum += diff;
-                count++;
-                start = now;
-                requestAnimationFrame(measureAnimationFrameIntervals);
-            }
-        }
+    //         if (start == null) {
+    //             start = performance.now();
+    //             requestAnimationFrame(measureAnimationFrameIntervals);
+    //         } else {
+    //             const now = performance.now();
+    //             const diff = now - start!;
+    //             sum += diff;
+    //             count++;
+    //             start = now;
+    //             requestAnimationFrame(measureAnimationFrameIntervals);
+    //         }
+    //     }
 
-        animationFrameRef.current = requestAnimationFrame(measureAnimationFrameIntervals);
+    //     animationFrameRef.current = requestAnimationFrame(measureAnimationFrameIntervals);
 
-        return () => {
-            if (animationFrameRef.current) {
-                cancelAnimationFrame(animationFrameRef.current);
-            }
-        };
-    }, []);
+    //     return () => {
+    //         if (animationFrameRef.current) {
+    //             cancelAnimationFrame(animationFrameRef.current);
+    //         }
+    //     };
+    // }, []);
 
     return (
         <Router>
--- src/store/use-app-store.tsx
+++ src/store/use-app-store.tsx
@@ -3,7 +3,7 @@ import { StudioActivationStatus, type AppState, type Loop, type LooperState, typ
 
 export const useAppStore = create<AppState>((set) => ({
     // general
-    activateStudio: StudioActivationStatus.LOADING,
+    activateStudio: StudioActivationStatus.ACTIVE,
     amps: [],
     intensity: { prev: 0, current: 0 },
     setActivateStudio: (activateStudio: StudioActivationStatus) => set({ activateStudio }),
